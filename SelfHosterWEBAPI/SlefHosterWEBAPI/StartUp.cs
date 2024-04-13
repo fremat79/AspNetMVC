1b9bd75938d2aa6825e20e1afd6598c94a282417
Matteo Freddi <matteo.freddi@hotmail.it>
2024-04-13 14:44:10 +0200

    Add self hosted WEBAPI example with Oauth Authentication

---
+++ SelfHosterWEBAPI/SlefHosterWEBAPI/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+namespace OwinSelfhostSample
+{
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                //AuthorizeEndpointPath = new PathString("/authorize"),
+                ApplicationCanDisplayErrors = true,
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+            appBuilder.UseOAuthAuthorizationServer(OAuthServerOptions);
+            appBuilder.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+
+
+            appBuilder.UseWebApi(config);
+            
+        }
+    }
+}
