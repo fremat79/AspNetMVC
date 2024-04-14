97b9f354168f2fdb7ffef1582cef4595012ccb0c
Matteo Freddi <matteo.freddi@hotmail.it>
2024-04-14 18:56:24 +0200

    Add example for win service

---
+++ SelfHosterWEBAPI/SelfHostedWEBAPIService/Service1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Owin.Hosting;
+using Newtonsoft.Json;
+using OwinSelfhostSample;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfHostedWEBAPIService
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        IDisposable _webApp;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        public static string BaseAddress => "http://localhost:9000/";
+
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Debugger.Launch();
+
+            // Start OWIN host 
+            _webApp = WebApp.Start<Startup>(url: BaseAddress);
+            
+            // Create HttpClient and make a request to api/values 
+            HttpClient client = new HttpClient();
+
+            var pairs = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", "admin"),
+                new KeyValuePair<string, string>("password", "123")
+            };
+
+            //Console.ReadLine();
+
+            var content = new FormUrlEncodedContent(pairs);
+
+            // Setup the request URL
+            var requestUri = $"{BaseAddress}token";
+
+            // Make the request
+            var token = client.PostAsync(requestUri, content).Result.Content.ReadAsStringAsync().Result;
+
+            Authoken aToken = JsonConvert.DeserializeObject<Authoken>(token);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", aToken.Token);
+
+            var response = client.GetAsync(BaseAddress + "api/values").Result;
+        }
+
+        protected override void OnStop()
+        {
+            _webApp?.Dispose();
+        }
+    }
+
+    public class Authoken
+    {
+        [JsonProperty(PropertyName = "access_token")]
+        public string Token { get; set; }
+
+        [JsonProperty(PropertyName = "token_type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "expires_in")]
+        public int ExipiresIn { get; set; }
+    }
+}
---
+++ SelfHosterWEBAPI/SelfHostedWEBAPIService/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OwinSelfhostSample
+{
+    internal class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {                      
+            // all client are always valdiate
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            // validate user
+
+            if ( context.UserName == "admin" && context.Password == "123")
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                //identity.AddClaim(new Claim("Email", user.UserEmailID));
+                context.Validated(identity);
+            }                           
+        }
+    }
+}
